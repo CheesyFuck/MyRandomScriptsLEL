40d346badd82204cf585fc8282b53ca258f68d10
DGJM <darylgenemata7@gmail.com>
2021-04-12 19:38:45 +0800

    added my first script:)

---
+++ kensurbot-in-dietpi-full.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#full installer for Kensurbot in dietpi(and possibly rasbian or any debian distro, not bothered to test so meh)
+#To anyone is here the way to install this is apt install -y curl dos2unix && curl https://pastebin.com/raw/M669vgvv | dos2unix | bash
+#it needs dos2unix cause i made this script in windowsXDDD
+
+apt update
+apt upgrade -y
+apt -y install git chromium-driver neofetch ffmpeg aria2 build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev nano libssl-dev libpq-dev libxml2-dev libxslt-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev libjpeg-dev curl software-properties-common
+cd /root/
+# python installer
+wget https://www.python.org/ftp/python/3.9.4/Python-3.9.4.tgz && tar -xf Python-3.9.4.tgz && cd Python-3.9.4 && ./configure --enable-optimizations && make -j 2 && make altinstall
+cd /root/
+git clone https://github.com/DGJM/KensurBot.git && cd KensurBot ; python3.9 -m pip install virtualenv && python3.9 -m virtualenv env && . ./env/bin/activate && pip install -r requirements.txt && ln -s ln -s /mnt/dietpi_userdata/config.env ./
+ln -s /root/KensurBot /mnt/dietpi_userdata/
+cd /root/
+echo '#Aria
+sleep 2
+aria2c --daemon=true --enable-rpc –rpc-listen-port 8210
+sleep 5
+#Kensurbot
+cd /root/KensurBot && . ./env/bin/activate && python -m userbot' > /usr/bin/userbot
+chmod +x /usr/bin/userbot
+echo '[Unit]
+Description=userbot
+
+[Service]
+Type=simple
+User=root
+RemainAfterExit=yes
+#WorkingDirectory=/usr/bin
+ExecStart=sh /usr/bin/userbot
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/userbot.service
+systemctl enable userbot
+systemctl start userbot
+echo -e "\e[0;35m[installer] Finished! \e[0m-- do \e[0;32msystemctl status userbot \e[0mif its running fine."
